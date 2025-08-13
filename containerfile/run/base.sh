e923169715c6079dbfd8f8c5aa0121e9b2af69fe
Logan Bennett <bennett.j.logan@gmail.com>
2025-08-12 19:10:28 -0700

    build base from ubuntu:18.04, install poldrack_fmri python3 from source

--- containerfile/run/base.sh
+++ containerfile/run/base.sh
@@ -9,12 +9,17 @@ export DEBIAN_FRONTEND=noninteractive
 apt-get -qq update 
 apt-get -qq -y upgrade
 apt-get -qq -y dist-upgrade
-apt-get -qq -y install apt-utils wget curl rsync git zip unzip p7zip man-db less vim ca-certificates
+apt-get -qq -y install apt-utils wget curl rsync git zip unzip p7zip man-db less vim ca-certificates gnupg
+ 
+wget -O- http://neuro.debian.net/lists/bionic.us-ca.full | tee /etc/apt/sources.list.d/neurodebian.sources.list
+
+apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 0xA5D32F012649A5A9
+
+apt-get update
+apt-get install -qq -y fsl ants
 
 # Get node and npm
 apt-get -qq -y install nodejs npm
-# Get ants
-apt-get -qq -y install ants
 
 # Install rclone
 curl -fsSL https://rclone.org/install.sh | bash
--- containerfile/run/poldrack_fmri.sh
+++ containerfile/run/poldrack_fmri.sh
@@ -6,9 +6,26 @@ set -o pipefail
 TMPDIR=/tmp
 export DEBIAN_FRONTEND=noninteractive
 
-# Update package lists and install python 3.13.5 and pip
+# Update package lists and install dependencies for compiling python 3.13.5 from source
 apt-get -qq update
-apt-get -qq -y install python3 python3-venv python3-pip
+apt-get -qq -y install build-essential libssl-dev zlib1g-dev libbz2-dev \
+    libreadline-dev libsqlite3-dev libncursesw5-dev xz-utils tk-dev \
+    libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev
+
+# Install Python 3.13.5 from source
+cd $TMPDIR
+curl -O https://www.python.org/ftp/python/3.13.5/Python-3.13.5.tgz
+tar -xzf Python-3.13.5.tgz
+cd Python-3.13.5
+./configure --enable-optimizations --prefix=/usr/local
+make -j$(nproc)
+make altinstall
+cd $TMPDIR
+rm -rf Python-3.13.5 Python-3.13.5.tgz
+ln -sf /usr/local/bin/python3.13 /usr/bin/python3
+curl https://bootstrap.pypa.io/get-pip.py -o $TMPDIR/get-pip.py
+python3 $TMPDIR/get-pip.py --break-system-packages
+rm $TMPDIR/get-pip.py
 
 pip install --break-system-packages \
     python-dotenv numpy pandas \
